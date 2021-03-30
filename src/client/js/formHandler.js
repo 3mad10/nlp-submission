504b62f78da5613ea7fc9502508de2f79143fd25
3mad10 <m.emad7798@gmail.com>
2021-03-30 22:36:38 +0200

    api test

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,6 +5,14 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
+    const formdata = {
+        "key":"359858ef84deeccf76da0de203d5f639",
+        "url":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this",
+        "lang":"en"
+    }
+    const response = fetch("https://api.meaningcloud.com/sentiment-2.1",formdata)
+    console.log(response)
+
     console.log("::: Form Submitted :::")
     fetch('http://localhost:3000/test')
     .then(res => res.json())
